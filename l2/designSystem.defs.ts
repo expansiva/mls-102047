1cc9d6a2a71ee5038dc020cee35c321770b5bb28
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-08-19 10:44:50 -0300

    clear

--- l2/designSystem.defs.ts
+++
@@ -1,2 +0,0 @@
-/// <mls fileReference="_102047_/l2/designSystem.defs.ts" enhancement="_blank"/>
-
--- l2/designSystem.test.ts
+++
@@ -1,2 +0,0 @@
-/// <mls fileReference="_102047_/l2/designSystem.test.ts" enhancement="_blank"/>
-
