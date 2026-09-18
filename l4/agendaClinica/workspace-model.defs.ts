1d69bc75d5c466164e98e5dcabecd5ce77c23a65
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-09-18 14:21:45 -0300

    publish: l4 (2 files)

---
+++ l4/agendaClinica/workspace-model.defs.ts
@@ -0,0 +1,12 @@
+/// <mls fileReference="_102047_/l4/agendaClinica/workspace-model.defs.ts" enhancement="_blank" />
+
+export const agendaClinicaWorkspaceModel = {
+  "menu": [
+    {
+      "workspaceId": "overview",
+      "label": "Revisão humana"
+    }
+  ]
+} as const;
+
+export default agendaClinicaWorkspaceModel;
